e4e08d8a4b45ef4f845eff761ddd3162ff62d70c
Abdunabi <abdunabiabduvahobov86@gmail.com>
2023-08-11 00:01:02 +0500

    notification added

--- src/pages/customer/notifications/index.jsx
+++ src/pages/customer/notifications/index.jsx
@@ -3,10 +3,13 @@ import './notifications.scss'
 import Loader from 'components/loader';
 import ErrorPage from 'components/errorPage';
 import { useTranslation } from 'react-i18next';
+import arrowLeft from "assets/icons/ArrowCircleLeftYellow.png";
+import { useNavigate } from 'react-router-dom';
 
 export const CustomerNotifications = () => {
   const {i18n} = useTranslation()
   const lang = i18n.language
+  const navigate = useNavigate()
 
   return (
     <GetAll queryKey={['users-notifications']} url={'/users-notifications'}>
@@ -17,18 +20,22 @@ export const CustomerNotifications = () => {
           console.log(items);
           return (
             <div className="container">
-              {
-                items?.map((notif, i) =>{
-                  return (
-                    <div className='users-notification__wrapper'>
-                      <h2>{notif[`title_${lang}`]}</h2>
-                      <p>{notif[`message_${lang}`]}</p>
-                    </div>
-                  )
-                })
-              }
+              <div
+                className="tariff__arrow-circle-left"
+                onClick={() => navigate(-1)}
+              >
+                <img src={arrowLeft} alt="" />
+              </div>
+              {items?.map((notif, i) => {
+                return (
+                  <div className="users-notification__wrapper">
+                    <h2>{notif[`title_${lang}`]}</h2>
+                    <p>{notif[`message_${lang}`]}</p>
+                  </div>
+                );
+              })}
             </div>
-          )
+          );
         }
       }
     </GetAll>
